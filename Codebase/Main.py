ae533e44e3f14f47649090b72271a83a5d6a3cd5
Sarika <sarika.padmashali@sjsu.edu>
2016-05-05 16:13:22 -0700

    Scoring_Update
    Scoring function for the computer vision - updated

--- Codebase/Main.py
+++ Codebase/Main.py
@@ -90,6 +90,68 @@ def processImage(pathToImageFile,dataSet,typeOfDataSet):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         
+    return dataSet.trainingDataSet,dataSet.testDataSet
+
+        
+def scoring(train,test):
+    
+    
+    #Sample testing data delete later
+   # test = {"testing": ((48.0, 60.0), (93.0, 59.0), (84.0, 88.5), (82.0, 105.0), 45.0111097397076, 45.9156835950419, 30.84234102658227, 56.4003546088143, 47.29693436154187, 49.0)}
+    
+    #The dictionary will store the confidence score for a particular test image when trained by a particular image
+    confidence_score ={}
+    
+    
+    #Iterating through each test data and calculating the confidence score for each data in the training set
+    for key_test,value_test in test.iteritems():  
+                
+                for key_train,value_train in train.iteritems():
+                    confi={}
+                    i=0;
+                    #confident increments when there is a matching attribute 
+                    confident=0;
+                    #The number of matching features gives a count of the number of features which are matched
+
+                    number_of_matching=0;
+                    
+                    
+                    for y in value_train:
+                        if(i<4):
+                            
+                            # Matching each feature and calculating the confidence score 
+                            
+                            if(y[0]==value_test[i][0] and y[1]==value_test[i][1]) :
+                                    confident = confident+10
+                                    number_of_matching=number_of_matching+1
+                            i=i+1
+                        else:
+                            if(y==value_test[i]):
+                                confident = confident+10
+                                number_of_matching=number_of_matching+1
+
+                            i=i+1
+                            
+                            
+                            
+                    confi[key_train] =[confident,number_of_matching]
+                    confidence_score[key_test]=confi
+                 
+                       
+    return confidence_score
+ 
+#Printing out the output to a text file output.txt            
+def print_output_file(output):
+    
+    with open ("output.txt","a") as fh:
+                for x,y in output.iteritems():
+                    for h,w in y.iteritems():
+                        out="Testing Image-->   %s   Training Image-->   %s   Confidence Score-->    %d    Number of Matching attributes-->   %d\n" % (x,h,w[0],w[1])
+                        fh.write(out)             
+    
+                        
+                         
+                                
 def calculateDistance(point1, point2, method="euclidean"):
     if method == "euclidean":
             return ( math.sqrt( pow( (point1[0] - point2[0]),2 ) + pow( (point1[1] - point2[1]),2 )) )
@@ -100,22 +162,29 @@ def main():
     
     trainingData = FaceRecognizer.ImageFeatureSet()
     testingData = FaceRecognizer.ImageFeatureSet()
-
+    test={}
+    train={}
     # Call the below method in a loop for every file in the training data set
     for files in os.walk('.\\TrainingImages\\'):
         for filename in files[2]:
-            processImage('.\\TrainingImages\\'+str(filename),trainingData,"Training Data")
+            train,test=processImage('.\\TrainingImages\\'+str(filename),trainingData,"Training Data")
 
     # Call the below method for extracting the features of the test image
     for files in os.walk('.\\TestImage\\'):
         for filename in files[2]:
-            processImage('.\\TestImage\\'+str(filename),testingData,"Testing Data")
+            train,test=processImage('.\\TestImage\\'+str(filename),testingData,"Testing Data")
 
     print "\n\n\nPrinting the training data: \n******************************\n "
     trainingData.printDataSet("Training Data")    
 
     print "\n\n\nPrinting the test data: \n******************************\n"
     testingData.printDataSet("Testing Data")
+    
+     #Scoring the test data with the training dataset
+    score=scoring(train,test)  
+    
+    #Printing to the output file
+    print_output_file(score)
 
 if __name__ == '__main__':
     main()
