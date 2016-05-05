98d114236082d73831844c81aceb4382c3e431f1
Vikash Raja Samuel Selvin <samuel-72@users.noreply.github.com>
2016-05-05 02:00:02 -0700

    Update Main.py
    Changes as of 5-5-2016
    *****************************
    
    Completed extracting and storing the features of the training images
    Completed extracting and storing the features of the test image
    Changed the Folder structure and Renamed the Images folder

--- Codebase/Main.py
+++ Codebase/Main.py
@@ -1,3 +1,4 @@
+import os
 import math
 import numpy as np
 import cv2
@@ -8,7 +9,8 @@ reload(FaceRecognizer)
 
 face_cascade = cv2.CascadeClassifier('.\\Cascades\\haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('.\\Cascades\\haarcascade_eye.xml')
-mouth_cascade = cv2.CascadeClassifier('.\\Cascades\\smiled_01.xml')
+#mouth_cascade = cv2.CascadeClassifier('.\\Cascades\\smiled_01.xml')
+mouth_cascade = cv2.CascadeClassifier('.\\Cascades\\haarcascade_smile.xml')
 nose_cascade = cv2.CascadeClassifier('.\\Cascades\\Nariz.xml')
 
 def selectCascades():
@@ -16,12 +18,12 @@ def selectCascades():
 
 
 
-def processImage(pathToImageFile,trainingData):
+def processImage(pathToImageFile,dataSet,typeOfDataSet):
     global face_cascade, eye_cascade, mouth_cascade, nose_cascade
     
     #Get the filename from the path - This will be the key for the DS storing the image features
     filename = pathToImageFile.split("\\")[len(pathToImageFile.split("\\"))-1].strip()
-    print "File Name : ", filename
+    print "File Being Processed : ", filename
     
     # Selecting the image and other setup for cv2
     img = cv2.imread(pathToImageFile)
@@ -29,17 +31,21 @@ def processImage(pathToImageFile,trainingData):
     cv2.imshow('Input Image',img)
     # Identify the face in the image
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    """
     print "Face is : ",faces
+    """
     # Capture the cascade data in detectMultiScale, then draw the rectangles
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         # Identify the eye, mouth and nose in the image
-        eyes = eye_cascade.detectMultiScale(roi_gray, minSize=(30,30))
-        mouth = mouth_cascade.detectMultiScale(roi_gray,minSize=(30,40))
-        nose = nose_cascade.detectMultiScale(roi_gray,minSize=(60,60))
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, minNeighbors=10, minSize=(20,28))
+        mouth = mouth_cascade.detectMultiScale(roi_gray,1.1, minNeighbors=10, minSize=(20,28))
+        nose = nose_cascade.detectMultiScale(roi_gray,1.1, minNeighbors=10, minSize=(20,28))
+        """
         print "Eyes are : ", eyes
+        """
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)        
         
@@ -55,7 +61,7 @@ def processImage(pathToImageFile,trainingData):
         rightEye = FaceRecognizer.FR_Eye(eyes[0])
         nose     = FaceRecognizer.FR_Nose(nose)
         mouth    = FaceRecognizer.FR_Mouth(mouth)
-        
+        """
         # Printing Debug information
         print "Leye Upper Left:        ", leftEye.upperLeftPoint
         print "Leye Upper Right:        ", leftEye.upperRightPoint
@@ -66,26 +72,25 @@ def processImage(pathToImageFile,trainingData):
         print "Reye Upper Right:        ", rightEye.upperRightPoint
         print "Reye Lower Left:        ", rightEye.lowerLeftPoint
         print "Reye Lower Right:        ", rightEye.lowerRightPoint                        
-
+        """
         # Calculating features
         distLeftRightEyeCenter  = calculateDistance(leftEye.center, rightEye.center)
         distLefEyeNoseCenter    = calculateDistance(leftEye.center, nose.center)
         distRightEyeNoseCenter  = calculateDistance(rightEye.center, nose.center)
         distLefEyeMouthCenter   = calculateDistance(leftEye.center, mouth.center)
         distRightEyeMouthCenter = calculateDistance(rightEye.center, mouth.center)
-        print "Distance : ", distLeftRightEyeCenter
+        
         #Storing features in object of class ImageFeatureSet
-        trainingData.addToTrainingDataSet(filename,leftEye.center,rightEye.center,nose.center,mouth.center,distLeftRightEyeCenter,distLefEyeNoseCenter,distRightEyeNoseCenter,distLefEyeMouthCenter,distRightEyeMouthCenter,face.width)
-        
-        # Printing details from the object
-        trainingData.printDataSet("Training Data")
-        
+        if (typeOfDataSet == "Training Data"):
+            dataSet.addToTrainingDataSet(filename,leftEye.center,rightEye.center,nose.center,mouth.center,distLeftRightEyeCenter,distLefEyeNoseCenter,distRightEyeNoseCenter,distLefEyeMouthCenter,distRightEyeMouthCenter,face.width)
+        elif (typeOfDataSet == "Testing Data"):
+            dataSet.addToTestDataSet(filename,leftEye.center,rightEye.center,nose.center,mouth.center,distLeftRightEyeCenter,distLefEyeNoseCenter,distRightEyeNoseCenter,distLefEyeMouthCenter,distRightEyeMouthCenter,face.width)
+                    
         cv2.imshow("Image with identification", img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         
 def calculateDistance(point1, point2, method="euclidean"):
-    print "Inside Dis function.\nP1 : ",point1,type(point1)
     if method == "euclidean":
             return ( math.sqrt( pow( (point1[0] - point2[0]),2 ) + pow( (point1[1] - point2[1]),2 )) )
     
@@ -94,10 +99,23 @@ def main():
     selectCascades()
     
     trainingData = FaceRecognizer.ImageFeatureSet()
-    # Call the below method in a loop for every file in the training data set
-    processImage('.\\Images\\test.jpg',trainingData)
-    
+    testingData = FaceRecognizer.ImageFeatureSet()
 
-    
+    # Call the below method in a loop for every file in the training data set
+    for files in os.walk('.\\TrainingImages\\'):
+        for filename in files[2]:
+            processImage('.\\TrainingImages\\'+str(filename),trainingData,"Training Data")
+
+    # Call the below method for extracting the features of the test image
+    for files in os.walk('.\\TestImage\\'):
+        for filename in files[2]:
+            processImage('.\\TestImage\\'+str(filename),testingData,"Testing Data")
+
+    print "\n\n\nPrinting the training data: \n******************************\n "
+    trainingData.printDataSet("Training Data")    
+
+    print "\n\n\nPrinting the test data: \n******************************\n"
+    testingData.printDataSet("Testing Data")
+
 if __name__ == '__main__':
     main()
